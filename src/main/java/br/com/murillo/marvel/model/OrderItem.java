5f400bc8f45efedf1a98c6b754cc5d915a6b71b7
Murillo Morgado <mumorgadok@hotmail.com>
2024-02-29 19:51:00 -0300

    subTotal e Total

--- src/main/java/br/com/murillo/marvel/model/OrderItem.java
+++ src/main/java/br/com/murillo/marvel/model/OrderItem.java
@@ -20,6 +20,8 @@ public class OrderItem implements Serializable {
   private Integer quantity;
   private Double price;
 
+
+
   public OrderItem() {
 
   }
@@ -64,6 +66,10 @@ public class OrderItem implements Serializable {
 
   public void setProduct(Product product){
     id.setProduct(product);
+  }
+
+  public Double getSubTotal(){
+    return price * quantity;
   }
 
   @Override
