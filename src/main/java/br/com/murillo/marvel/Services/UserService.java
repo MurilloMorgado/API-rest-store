5082ae6268020f7c8692cb91ffaf26db6163e46d
Murillo Morgado <mumorgadok@hotmail.com>
2024-03-04 20:11:26 -0300

    exception

--- src/main/java/br/com/murillo/marvel/Services/UserService.java
+++ src/main/java/br/com/murillo/marvel/Services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.murillo.marvel.Repository.UserRepository;
+import br.com.murillo.marvel.Services.Exception.ResourceNotFoundException;
 import br.com.murillo.marvel.model.User;
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 
   public User findById(Long id){
     Optional<User> obj = repository.findById(id);
-    return obj.get();
+    return obj.orElseThrow(() -> new ResourceNotFoundException(id));
   }
 
   public User insert(User obj){
